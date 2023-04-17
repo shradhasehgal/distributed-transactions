2106071eb94b925210d9134556ebfd6f306f3637
Chaitanya B Bhandari <chaitanyabhushanbhandari@gmail.com>
2023-04-17 10:08:19 -0500

    Getting rid of some redundant code that was copied over from MP1.

--- server/node.go
+++ server/node.go
@@ -404,10 +404,7 @@ func (s *distributedTransactionsServer) AbortCoordinator(ctx context.Context, pa
 }
 func (s *distributedTransactionsServer) AbortPeer(ctx context.Context, payload *protos.TxnIdPayload) (*protos.Reply, error) {
 	logrusLogger.WithField("node", currNodeName).Debug("Aborting transaction ", payload.TxnId)
-
 	timestampedConcurrencyID := s.txnIDToTimestampedConcurrencyID.M[payload.TxnId]
-	// commitChan := s.txnIDToChannel.M[timestampedConcurrencyID]
-	// commitChan <- true
 
 	s.objectNameToStatePtr.Mu.RLock()
 	defer s.objectNameToStatePtr.Mu.RLock()
@@ -490,82 +487,6 @@ func (s *distributedTransactionsServer) PreparePeer(ctx context.Context, payload
 	return &protos.Reply{Success: success}, nil
 }
 
-// func handleTxnMsg(msg *Message, received *SafeReceivedMap, nodeToEncoder *SafeEncoderMap, p *SafeMaxPriority, msgIDToLocalPriority *SafeMsgIDToLocalPriorityMap, safeIsisPq *SafePriorityQueue, msgIDToTransaction *SafeMsgIDToTransaction) {
-// 	txn, _ := msg.Payload.(Transaction)
-// 	var msgID = txn.MsgID
-// 	if !isReceived(received, msgID) {
-// 		var maxPo = getPriorityAndIncrement(p)
-// 		var proposalMsg = Message{}
-// 		proposalMsg.Type = "proposal"
-// 		proposalMsg.Payload = &Proposal{ProposedPriority: maxPo, ProposingNode: currNodeName, MsgID: msgID}
-// 		logrusLogger.WithField("node", currNodeName).Debug("Received message with ID ", msgID, " from ", txn.CurSenderNode)
-// 		logrusLogger.WithField("node", currNodeName).Debug("Unicasting proposal message with priority", maxPo, " to ", txn.SenderNode, "for TxN with message ID: ", msgID)
-// 		var localPriority = &LocalPriority{Priority: maxPo, nodeID: currNodeName, msgID: msgID, isAccepted: false}
-// 		setMsgIDToTransaction(msgIDToTransaction, msgID, &txn)
-
-// 		safeIsisPq.Push(localPriority)
-// 		setMsgIDToLocalPriority(msgIDToLocalPriority, msgID, localPriority)
-// 		unicast(getEncoder(nodeToEncoder, txn.SenderNode), proposalMsg)
-// 		if txn.SenderNode != currNodeName {
-// 			txn.CurSenderNode = currNodeName
-// 			msg.Payload = txn
-// 			logrusLogger.WithField("node", currNodeName).Debug("Resending message with ID ", msgID)
-// 			basicMulticast(nodeToEncoder, msg)
-// 		}
-// 	}
-// }
-
-// func handleProposalMsg(msg *Message, msgToChannel *SafeMsgIDToChannelMap) {
-// 	proposal, _ := msg.Payload.(Proposal)
-// 	logrusLogger.WithField("node", currNodeName).Debug("Received proposal message from %s", proposal.ProposingNode, " with priority %d", proposal.ProposedPriority, "for message ID: ", proposal.MsgID)
-// 	setMsgIDToChannel(msgToChannel, &proposal)
-// }
-
-// func handleAcceptMsg(msg *Message, msgIDToLocalPriority *SafeMsgIDToLocalPriorityMap, safeIsisPq *SafePriorityQueue, deliveryLogger *log.Logger, msgIDtoTransaction *SafeMsgIDToTransaction, accountsToBalances *SafeAccountsToBalances, txnLogger *log.Logger, nodeToEncoder *SafeEncoderMap, measurementsLogger *log.Logger) {
-// 	acceptance, _ := msg.Payload.(Acceptance)
-// 	var acceptedMsgID = acceptance.MsgID
-// 	var acceptedPriority = acceptance.AcceptedPriority
-// 	var acceptedNode = acceptance.AcceptedNode
-// 	logrusLogger.WithField("node", currNodeName).Debug("Received acceptance message with priority ", acceptedPriority, ".", acceptedNode, " for messageID: ", acceptedMsgID)
-
-// 	safeIsisPq.Update(getMsgIDLocalPriority(msgIDToLocalPriority, acceptedMsgID), acceptedPriority, acceptedNode)
-// 	safeIsisPq.HandleDeliverable(deliveryLogger, msgIDtoTransaction, accountsToBalances, txnLogger, nodeToEncoder, measurementsLogger)
-// }
-
-// func handleConnection(connection net.Conn, received *SafeReceivedMap, nodeToEncoder *SafeEncoderMap, p *SafeMaxPriority, msgToChannel *SafeMsgIDToChannelMap, msgIDToLocalPriority *SafeMsgIDToLocalPriorityMap, safeIsisPq *SafePriorityQueue, deliveryLogger *log.Logger, msgIDtoTransaction *SafeMsgIDToTransaction, accountsToBalances *SafeAccountsToBalances, txnLogger *log.Logger, measurementsLogger *log.Logger) {
-// func handleConnection(connection net.Conn, nodeToEncoder *utils.SafeEncoderMap) {
-
-// 	defer connection.Close()
-// 	logrusLogger.WithField("node", currNodeName).Debug("Connection from ", connection.RemoteAddr().String())
-// 	// decoder := gob.NewDecoder(connection)
-// 	// connectingNode := ""
-// 	for {
-// 		// var msg Message
-// 		// err := decoder.Decode(&msg)
-// 		// if err != nil {
-// 		// 	if err == io.EOF {
-// 		// 		logrusLogger.WithField("node", currNodeName).Debug(connectingNode, " has failed!")
-// 		// 		deleteNode(nodeToEncoder, connectingNode)
-// 		// 		return
-// 		// 	}
-// 		// 	logrusLogger.WithField("node", currNodeName).Error("Decode error: ", err)
-// 		// } else {
-// 		// 	if msg.Type == "transaction" {
-// 		// 		handleTxnMsg(&msg, received, nodeToEncoder, p, msgIDToLocalPriority, safeIsisPq, msgIDtoTransaction)
-// 		// 	} else if msg.Type == "proposal" {
-// 		// 		handleProposalMsg(&msg, msgToChannel)
-// 		// 	} else if msg.Type == "acceptance" {
-// 		// 		handleAcceptMsg(&msg, msgIDToLocalPriority, safeIsisPq, deliveryLogger, msgIDtoTransaction, accountsToBalances, txnLogger, nodeToEncoder, measurementsLogger)
-// 		// 	} else if msg.Type == "registration" {
-// 		// 		registration, _ := msg.Payload.(Registration)
-// 		// 		connectingNode = registration.NodeID
-// 		// 	}
-
-// 		// }
-
-// 	}
-// }
-
 // Starts listening on all available unicast and anycast IP addresses
 // of the local system, at the specified port.
 func listenOnPort(port string) (net.Listener, error) {
@@ -577,92 +498,6 @@ func listenOnPort(port string) (net.Listener, error) {
 	}
 	return listener, nil
 }
-
-// func sendMsg(encoder *gob.Encoder, msg Message, wg *sync.WaitGroup) {
-// 	defer wg.Done()
-// 	err := encoder.Encode(msg)
-// 	if err != nil {
-// 		logrusLogger.WithField("node", currNodeName).Error("Encode error: ", err)
-// 	}
-// }
-
-// func unicast(encoder *gob.Encoder, msg Message) {
-// 	err := encoder.Encode(msg)
-// 	if err != nil {
-// 		logrusLogger.WithField("node", currNodeName).Error("Encode error: 	", err)
-// 	}
-// }
-
-// func proposalGreater(p1 *Proposal, p2 *Proposal) bool {
-// 	p1Node, _ := strconv.Atoi(p1.ProposingNode[4:])
-// 	p2Node, _ := strconv.Atoi(p2.ProposingNode[4:])
-// 	var p1Priority = p1.ProposedPriority
-// 	var p2Priority = p2.ProposedPriority
-// 	return (p1Priority > p2Priority) || (p1Priority == p2Priority && p1Node > p2Node)
-// }
-
-// func processTxn(nodeToEncoder *SafeEncoderMap, msg Message, proposals chan *Proposal, totalNodes int) {
-// 	var msgID = msg.Payload.(Transaction).MsgID
-
-// 	go basicMulticast(nodeToEncoder, &msg)
-// 	var maxProposal = &Proposal{ProposedPriority: 0, ProposingNode: "node0"}
-// 	var breakOut = false
-// 	var receivedFrom []string
-// 	for {
-// 		select {
-// 		case proposal := <-proposals:
-// 			receivedFrom = append(receivedFrom, proposal.ProposingNode)
-// 			if proposalGreater(proposal, maxProposal) {
-// 				maxProposal = proposal
-// 			}
-// 			/*
-// 				Imagine there are total 3 nodes -> A, B, C. We have received
-// 				proposals from A & B. Suppose we enter this loop having received B's proposal,
-// 				B dies when we enter the loop and nodeToEncoder now has only A & C.
-// 				When we enter receivedFromAllAliveNodes, we will get False because
-// 				numReceivedFromAlive will be 1 (for A) and totalAliveNodes will 2 (For A & C).
-// 				totalAliveNodes will not change when we're in receivedFromAllAliveNodes.
-// 			*/
-// 			if receivedFromAllAliveNodes(nodeToEncoder, &receivedFrom) {
-// 				breakOut = true
-// 				break
-// 			}
-// 		case <-time.After(15 * time.Second):
-// 			logrusLogger.WithField("node", currNodeName).Debug("Hit timeout while waiting for proposals on message ID: ", msgID)
-// 			breakOut = true
-// 			break
-// 		}
-// 		if breakOut {
-// 			break
-// 		}
-// 	}
-// 	logrusLogger.WithField("node", currNodeName).Debug("Accepted proposal with priority ", maxProposal.ProposedPriority, " from ", maxProposal.ProposingNode, "for message ID ", maxProposal.MsgID)
-
-// 	var AcceptanceMsg = Message{}
-// 	AcceptanceMsg.Type = "acceptance"
-// 	var accepted = Acceptance{AcceptedPriority: maxProposal.ProposedPriority, AcceptedNode: maxProposal.ProposingNode, MsgID: maxProposal.MsgID}
-// 	AcceptanceMsg.Payload = accepted
-
-// 	go basicMulticast(nodeToEncoder, &AcceptanceMsg)
-// }
-
-// func basicMulticast(nodeToEncoder *SafeEncoderMap, msg *Message) {
-// 	var wg sync.WaitGroup
-// 	defer nodeToEncoder.mu.RUnlock()
-// 	nodeToEncoder.mu.RLock()
-// 	for _, encoder := range nodeToEncoder.m {
-// 		wg.Add(1)
-// 		go sendMsg(encoder, *msg, &wg)
-// 	}
-// 	wg.Wait()
-// }
-
-// func initGob() {
-// 	gob.Register(Registration{})
-// 	gob.Register(Transaction{})
-// 	gob.Register(Proposal{})
-// 	gob.Register(Acceptance{})
-// }
 
 func main() {
 	arguments := os.Args
